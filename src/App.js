4947429ca7aa24806f6c92123319de8de8840bb8
NaRae-tech <narae.dev@gmail.com>
2021-10-07 23:17:32 +0900

    finish react and javascript

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
 import React from 'react';
 import axios from "axios";
-import Movie from "./Movie"; 
+import Movie from "./Movie";
+import "./App.css";
 
-class App extends React.Component{
-  state={
-    isLoading:true,
-    movies:[]
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   };
-  getMovies = async() =>{
+  getMovies = async () => {
     const {
       data: {
         data: { movies }
       }
     } = await axios.get(
       "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
-      );
-    this.setState({movies,isLoading:false});
+    );
+    this.setState({ movies, isLoading: false });
   };
-  componentDidMount(){
+  componentDidMount() {
     this.getMovies();
   }
-  render(){
-    const {isLoading, movies} = this.state;
+  render() {
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-            />
-          ))
-        }
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading..</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
